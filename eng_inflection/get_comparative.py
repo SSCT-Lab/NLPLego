5ab0047197567fcf1313aed3fdf240382363c313
PhyllisJi <34181680+PhyllisJi@users.noreply.github.com>
2022-09-01 15:30:02 +0800

    Add files via upload

---
+++ eng_inflection/get_comparative.py
@@ -0,0 +1,169 @@
+#-*- encoding: utf-8 -*-
+# 2 syllables are irregular (sometimes both -er and more are used)
+# 2 syllables ends with y, ly, lly are most irregular (silly, fully)
+# there are none-comparable adjectives and adverbs (wiktionary)
+# added functionality of finding positive when input is conparative of superlative
+# may not work correctly when positive ends with e > lets provide two possible positives
+
+import os, sys
+import pdb
+from eng_inflection.define_comparative import *
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+def find_positive_irregular(word):
+  for key in irregular_tagged_comps:
+    if word == key: return key
+    comp_list = irregular_tagged_comps[key]
+    for comp in comp_list:
+      if word == comp[3:]: return key 
+  return ""
+
+# snug, snugger, snuggest
+def doubled_consonant(candidate):
+  if count_syllable(candidate) == 1 and len(candidate) > 3 and candidate[-1] == candidate[-2]:
+    word = candidate[:-1]
+    if word[-1] in consonants and word[-1] != 'x' and word[-1] != 'w' and word[-2] in vowels and word[-3] in consonants:
+      return True
+  return False
+
+# positive means the ordinary form of adverb/adjective
+def find_positive_regular(word):
+  positive = ""
+  positives = []
+  if word.endswith('iest'):
+    positive = word[:-4] + 'y'
+  elif word.endswith('ier'):
+    positive = word[:-3] + 'y'
+  elif word.endswith('est'):
+    positive1 = word[:-3]
+    positive2 = word[:-2]
+    if doubled_consonant(positive1): positive1 = positive1[:-1]
+    if doubled_consonant(positive2): positive2 = positive2[:-1]
+    positives = [positive1, positive2]
+  elif word.endswith('er'):
+    positive1 = word[:-2]
+    positive2 = word[:-1]
+    if doubled_consonant(positive1): positive1 = positive1[:-1]
+    if doubled_consonant(positive2): positive2 = positive2[:-1]
+    positives = [positive1, positive2]
+  else:
+    return ""
+
+  if positive != "":
+    comp_list = get_regular_comp(positive)
+    for comp in comp_list:
+      if word == comp[3:]: return positive
+  elif len(positives) > 0:
+    for positive in positives:
+      comp_list = get_regular_comp(positive)
+      for comp in comp_list:
+        if word == comp[3:]: return positive
+  return ""
+
+# wrong counting but good for comparative: quiet, easy
+# wrong counting: fine
+def count_syllable(word):
+  num_syll = 0
+  is_vowel = False
+  was_vowel = False
+  for ch in word:
+    if ch in vowels or ch == 'y':
+      is_vowel = True
+    else:
+      is_vowel = False
+    if is_vowel and was_vowel == False:
+      num_syll += 1
+    was_vowel = is_vowel
+  if word[-1] == 'e' and word[-2] in consonants:
+    num_syll -= 1
+  return num_syll
+
+def get_irregular_comp(word):
+  if word in irregular_tagged_comps:
+    return irregular_tagged_comps[word]
+  else:
+    return None
+
+def regular_short_comp(word):
+  if word.endswith('e'):
+    return ['CO:'+word+'r', 'SU:'+word+'st']
+  else:
+    return ['CO:'+word+'er', 'SU:'+word+'est']
+
+# bloodier, sillier, chillier, more fully, holier, more briefly, uglier
+# ly is irregular: holier should be defined as irregular
+# length may be used for regulation
+def ends_y(word):
+  if word[-1] == 'y':
+    if word.endswith('illy'):
+      return ['CO:'+word[:-1]+'ier', 'SU:'+word[:-1]+'iest']
+    elif word.endswith('ly'):
+      if len(word) <= 4:
+        return ['CO:'+word[:-1]+'ier', 'SU:'+word[:-1]+'iest']
+      else:
+        return []
+    elif word[-2] in consonants:
+      return ['CO:'+word[:-1]+'ier', 'SU:'+word[:-1]+'iest']
+    else:
+      return ['CO:'+word+'er', 'SU:'+word+'est']
+  else:
+    return None
+
+def ends_single_consonants(word):
+  if word[-1] in consonants:
+    if len(word) < 3: return None # up, in are irregular
+    if word.endswith('w') or word.endswith('x'):
+      return ['CO:'+word+'er', 'SU:'+word+'est']
+    elif word[-2] in vowels and word[-3] not in vowels: # double the last consonant
+      return ['CO:'+word+word[-1]+'er', 'SU:'+word+word[-1]+'est']
+    else:
+      return ['CO:'+word+'er', 'SU:'+word+'est']
+  else:
+    return None
+
+def get_regular_comp(word):
+  comp = ends_y(word)
+  if comp == None and count_syllable(word) < 2:
+    comp = ends_single_consonants(word)
+    if comp == None: comp = regular_short_comp(word)
+  if comp == None: comp = []
+  return comp
+
+def get_comparative(word):
+  if word in poly_comps:
+    positive = word
+  else:
+    positive = find_positive_irregular(word)
+    if positive == "":
+      positive = find_positive_regular(word)
+      if positive == "":
+        positive = word
+
+  comp = get_irregular_comp(positive)
+  if comp == None:
+    num_syll = count_syllable(positive)
+    #print "not in irregular list, number of syllble is", num_syll
+    if num_syll > 2:
+      comp = []
+    else:
+      if positive.endswith('ous') or positive.endswith('ful') or positive.endswith('ed') or positive.endswith('ing') \
+        or positive.endswith('est') or positive.endswith('less') or positive.endswith('able') or positive.endswith('ant') \
+        or positive.endswith('ive') or positive.endswith('ent') or positive.endswith('free'):
+        comp = []
+      else:
+        comp = get_regular_comp(positive)
+  return positive, comp
+
+vowels = ['a','e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'q', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+poly_comps = ['eager', 'bitter', 'silver', 'tender', 'slender', 'sheer', 'sober', 'clever', 'proper']
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print ("need an adjective or adverb to get comparative/superlative")
+    sys.exit()
+  word = sys.argv[1]
+
+  positive, comp = get_comparative(word)
+  print (positive, comp)
---
+++ eng_inflection/get_conjugation.py
@@ -0,0 +1,149 @@
+#-*- encoding: utf-8 -*-
+# generate all conjugation of a verb/auxiliary_verb/modal_verb
+# must >> 'had to' is no conjugation
+# added consonant doubling - jam
+# unconjugatable word should be covered by irregular (must, ought)
+# added functionality of finding infinitive when conjugated word in given
+# check for the case when conjugated spell is the same as infinitive: fell/fall, found/find, lay/lie
+# added show tenses with each conjugation (TS:third singular, PC:present continous, PA:past, PP:past participle)
+
+import re
+import os, sys
+import pdb
+from eng_inflection.get_comparative import count_syllable
+from eng_inflection.define_conjugation import *
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+def find_infinitive_irregular(word):
+  for key in irregular_tagged_conjus:
+    if word == key: return key
+    conju_list = irregular_tagged_conjus[key]
+    for conju in conju_list:
+      if word == conju[3:]: return key
+  return ""
+
+def doubled_consonant(candidate):
+  if count_syllable(candidate) == 1 and len(candidate) > 3 and candidate[-1] == candidate[-2]:
+    word = candidate[:-1]
+    if word[-1] in consonants and word[-1] != 'x' and word[-1] != 'w' and word[-2] in vowels and word[-3] in consonants:
+      return True
+  return False
+
+# -ying should be covered by irregular conjugation
+# -es, -ies, -ied should be covered by regular conjugation (kisses, fries, fried)
+def find_infinitive_regular(word):
+  if word.endswith('ing'):
+    infinitive = word[:-3]
+    if doubled_consonant(infinitive): infinitive = infinitive[:-1]
+  elif word.endswith('ied'):
+    infinitive = word[:-3] + 'y'
+  elif word.endswith('ed'):
+    infinitive = word[:-2]
+    if doubled_consonant(infinitive): infinitive = infinitive[:-1]
+  elif word.endswith('ies'):
+    infinitive = word[:-3] + 'y'
+  elif word.endswith('es'):
+    infinitive = word[:-2]
+  elif word.endswith('s'):
+    infinitive = word[:-1]
+  else:
+    return ""
+  conju_list = get_regular_conju(infinitive)
+  for conju in conju_list:
+    if word == conju[3:]: return infinitive
+  return ""
+
+def get_irregular_conju(word):
+  if word in irregular_tagged_conjus:
+    return irregular_tagged_conjus[word]
+  else:
+    return None
+
+def most_regular(word):
+  return ['TS:'+word+'s', 'PC:'+word+'ing', 'PA:'+word+'ed', 'PP:'+word+'ed']
+
+# rule for doubling consonant
+# two or more syllables should treated as irregular conjugation
+def regular_one_syllable(word):
+  if count_syllable(word) == 1 and len(word) > 2:
+    if word[-1] in consonants and word[-1] != 'x' and word[-1] != 'w' and word[-2] in vowels and word[-3] in consonants:
+      return ['TS:'+word+'s', 'PC:'+word+word[-1]+'ing', 'PA:'+word+word[-1]+'ed', 'PP:'+word+word[-1]+'ed']
+  return None
+
+def regular_endswith_e(word):
+  if word[-1] == 'e':
+    if word[-2] in ['e', 'y', 'o']: # free, dye, tiptoe
+      return ['TS:'+word+'s', 'PC:'+word+'ing', 'PA:'+word+'d', 'PP:'+word+'d']
+    elif word[-2] == 'i': # die, tie
+      return ['TS:'+word+'s', 'PC:'+word[:-2]+'ying', 'PA:'+word+'d', 'PP:'+word+'d']
+    else: # sue, love
+      return ['TS:'+word+'s', 'PC:'+word[:-1]+'ing', 'PA:'+word+'d', 'PP:'+word+'d']
+  else:
+    return None
+
+def regular_endswith_y(word):
+  if word[-1] == 'y':
+    if word[-2] in consonants:  # study, fly, carry, cry, worry, reply
+      return ['TS:'+word[:-1]+'ies', 'PC:'+word+'ing', 'PA:'+word[:-1]+'ied', 'PP:'+word[:-1]+'ied']
+    else: # enjoy, play, stay
+      return most_regular(word)
+  else:
+    return None
+
+# put es instead of s for 3rd person singular for a verb ends with s(ss), sh, ch, x, z, o
+# kiss, buzz, box, polish, preach, solo
+def regular_third_singular_es(word):
+  if word.endswith('ch') or word.endswith('sh') or word[-1] == 'x' or word[-1] == 's' or word[-1] == 'z' or word[-1] == 'o':
+    return ['TS:'+word+'es', 'PC:'+word+'ing', 'PA:'+word+'ed', 'PP:'+word+'ed']
+  else:
+    return None
+
+# Verbs with more than one syllable ending in l double the final letter before suffixing in Britain English
+# cancel > cancelled (US canceled)
+# handled as irregular conjugation
+def ends_l(word):
+  pass
+
+def regular_endswith_c(word):
+  if word.endswith('c'): # panic, mimic, traffic, picnic
+    return ['TS:'+word+'s', 'PC:'+word+'king', 'PA:'+word+'ked', 'PP:'+word+'ked']
+  else:
+    return None
+
+def get_regular_conju(word):
+  conju = regular_endswith_e(word)
+  if conju == None: conju = regular_endswith_y(word)
+  if conju == None: conju = regular_third_singular_es(word)
+  if conju == None: conju = regular_endswith_c(word)
+  if conju == None: conju = regular_one_syllable(word)
+  if conju == None: conju = most_regular(word)
+  return conju
+
+def get_conjugation(word):
+  if word in poly_conjus or word in irregular_tagged_conjus:
+    infinitive = word
+  else:
+    infinitive = find_infinitive_irregular(word)
+    if infinitive == "":
+      if word.endswith('s'):
+        infinitive = word
+      else:
+        infinitive = find_infinitive_regular(word)
+      if infinitive == "":
+        infinitive = word
+  conju = get_irregular_conju(infinitive)
+  if conju == None: conju = get_regular_conju(infinitive)
+  return infinitive, conju
+
+vowels = ['a','e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'q', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+poly_conjus = ['found', 'fell', 'lay', 'bind', 'saw', 'ground', 'wound', 'rent', 'resent']
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print ("need a verb to get conjugation")
+    sys.exit()
+  word = sys.argv[1]
+  infinitive, conju = get_conjugation(word)
+  print (infinitive, conju)
